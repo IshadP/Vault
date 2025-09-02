d264f8affa48f0073e8a952bcd2f4b31cbd8c6c6
Ishad Pande <ishadpande16@gmail.com>
2025-09-02 19:20:03 +0530

    vault backup: 2025-09-02 19:20:03

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["Private", "Templates", ".obsidian", "999_Attachments"],
+    ignorePatterns: ["Private", "Templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
